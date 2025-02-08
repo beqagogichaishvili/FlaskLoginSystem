a8accb1cda76d5cdc5db042e61305b023a56b10b
beqagogichaishvili <gogichaishvilibeqa69@gmail.com>
2025-02-08 21:27:51 +0400

    Update run.py

--- run.py
+++ run.py
@@ -3,38 +3,37 @@ from flaskapp import app, login_required
 from flaskapp.user.routes import *
 from flaskapp.user.models import *
 
-def run():
-  @app.route('/')
-  def home():
-    return render_template('home.html')
+@app.route('/')
+def home():
+  return render_template('home.html')
 
-  @app.route('/Homepage')
-  @login_required
-  def loggedhome():
-    return render_template('loggedhome.html')
+@app.route('/Homepage')
+@login_required
+def loggedhome():
+  return render_template('loggedhome.html')
 
-  @app.route('/about')
-  def about():
-    return render_template('about.html')
+@app.route('/about')
+def about():
+  return render_template('about.html')
 
-  @app.route('/contact')
-  def contact():
-    return render_template('contact.html')
+@app.route('/contact')
+def contact():
+  return render_template('contact.html')
 
-  @app.route('/register')
-  def register():
-    return render_template('register.html')
+@app.route('/register')
+def register():
+  return render_template('register.html')
 
-  @app.route('/loginuser')
-  def loginuser():
-    return render_template('login.html')
+@app.route('/loginuser')
+def loginuser():
+  return render_template('login.html')
 
-  @app.route('/dashboard/')
-  @login_required
-  def dashboard():
-    return render_template('dashboard.html')
+@app.route('/dashboard/')
+@login_required
+def dashboard():
+  return render_template('dashboard.html')
 
-  if __name__ == '__main__':
-    app.run(debug=False)
+if __name__ == '__main__':
+  app.run()
 
-  return app
+
